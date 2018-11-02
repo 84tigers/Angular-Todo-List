950b23c23f0942633fce6289b0dba9d5e97f3a40
84tigers <ahyska@gmail.com>
2018-11-01 21:47:40 -0400

    basic functions complete: add to page

--- controller.js
+++ controller.js
@@ -1,7 +1,18 @@
 "use strict";
 function TodoController() {
+    const vm = this;
+    vm.todos = [];
+    vm.addTodo = (newTodo) => {
+        console.log(newTodo);
+        vm.todos.push({
+            task: newTodo.task,
+            complet: newTodo.complete
+        });
+        vm.todos.push(angular.copy(newTodo));
+        
+    };
 
 }
-andular
+angular
     .module("App")
     .controller("TodoController", TodoController);
