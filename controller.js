1f6be8da668873442b6f0cf9f6fa20ce2b1068e2
84tigers <ahyska@gmail.com>
2018-11-03 20:33:25 -0400

    added missing required styling, line-through for completed, fixed filter

--- controller.js
+++ controller.js
@@ -1,7 +1,19 @@
 "use strict";
 function TodoController() {
     const vm = this;
-    vm.todos = ["Feed the Apatosaurus", "Clean the Stegasaurus litter", "Prove the existence of Brontosaurus", "Lock T-rex gate"];
+    vm.todos = [{
+        task: "Feed the Apatosaurus",
+        complete: false
+    }, {
+        task: "Clean the Stegasaurus litter", 
+        complete: true
+    }, {
+        task: "Prove the existence of Brontosaurus", 
+        complete: false
+    }, {
+        task: "Lock T-rex gate", 
+        compplete: false
+    }];
     vm.addTodo = (newTodo) => {
         console.log(newTodo);
         vm.todos.push({
